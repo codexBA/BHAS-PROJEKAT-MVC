9abe8f9571603d6d15e02a6236c32852a4153ba9
Erkan Islamovic <erkan@codex.ba>
2026-03-17 12:02:46 +0100

    Init

---
+++ BHAS/Controllers/DepartmentController.cs
@@ -0,0 +1,38 @@
+﻿using BHAS.DbFirst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHAS.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        /// <summary>
+        /// Vraca listu svih odjela iz baze i proslijedjuje u view
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            using (BHAS.DbFirst.StateStatisticsDBEntities db = new StateStatisticsDBEntities())
+            {
+                List<BHAS.DbFirst.Department> dpList
+                    = db.Departments.ToList();
+                //
+                return View(dpList);
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new StateStatisticsDBEntities())
+            {
+                var dpt = db.Departments
+                    .FirstOrDefault(x => x.DepartmentID == id);
+
+                return View(dpt);
+            }
+        }
+    }
+}
