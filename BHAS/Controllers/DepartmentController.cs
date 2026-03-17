fbdca0b8bc32bd0438a49f036c4e1a9916146858
Erkan Islamovic <erkan@codex.ba>
2026-03-17 15:23:53 +0100

    [IMPROVEMENT] Dodali smo Employee controller. Detalje zaposlenika, itd

--- BHAS/Controllers/DepartmentController.cs
+++ BHAS/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using BHAS.DbFirst;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,13 +25,21 @@ namespace BHAS.Controllers
             }
         }
 
+
+        /// <summary>
+        /// Prikaz detalja odjela - ukljucuje i poveznicu sa employee/manager
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult Details(int id)
         {
             using (var db = new StateStatisticsDBEntities())
             {
                 var dpt = db.Departments
+                    .Include(x => x.Employee)
+                    .Include(x => x.Employees)
                     .FirstOrDefault(x => x.DepartmentID == id);
-
+                //
                 return View(dpt);
             }
         }
---
+++ BHAS/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHAS.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // Ne koristi se trenutno
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Prikaz detalja zaposlenika
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Details(int id)
+        {
+            using (var db = new BHAS.DbFirst.StateStatisticsDBEntities())
+            {
+                // postoji razlika izmedju First i FirstOrDefault
+                var zaposleni = db.Employees.FirstOrDefault(e => e.EmployeeID == id);
+                return View(zaposleni);
+            }
+        }
+    }
+}
