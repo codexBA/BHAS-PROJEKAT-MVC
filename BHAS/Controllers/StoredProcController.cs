51ceb03c9bf857350f9b1d1378cdfb2c1d0c02bd
Erkan Islamovic <erkan@codex.ba>
2026-03-19 00:03:57 +0100

    [FEATURE] Dodan StoredProcController i nova stranica za Stored Procedure. Izmjene na home page-u za bolju navigaciju.

---
+++ BHAS/Controllers/StoredProcController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHAS.Controllers
+{
+    public class StoredProcController : Controller
+    {
+        // GET: StoredProc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
