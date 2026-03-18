9146f8b2cf7c7f5d6f54725af2465ac96c738a39
Erkan Islamovic <erkan@codex.ba>
2026-03-18 10:55:20 +0100

    [FEATURE] Dodali smo opciju kreiranja novog zaposlenog

--- BHAS/Controllers/EmployeeController.cs
+++ BHAS/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BHAS.DbFirst;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -24,9 +25,47 @@ namespace BHAS.Controllers
             using (var db = new BHAS.DbFirst.StateStatisticsDBEntities())
             {
                 // postoji razlika izmedju First i FirstOrDefault
-                var zaposleni = db.Employees.FirstOrDefault(e => e.EmployeeID == id);
+                var zaposleni = db.Employees
+                                        .FirstOrDefault(e => e.EmployeeID == id);
                 return View(zaposleni);
             }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                // podaci su ispravni => snimi u bazu
+                using (var db = new StateStatisticsDBEntities())
+                {
+                    // definisemo vrijednosti koje nisu odredjene kroz formu
+                    model.CreatedDate = DateTime.Now;
+                    model.ModifiedDate = DateTime.Now;
+                    // dodavanje
+                    try
+                    {
+                        db.Employees.Add(model);
+                        // snimanje
+                        db.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        // treba zapisati log
+                        return View(model);
+                    }
+                }
+
+                return RedirectToAction("Details", "Department", new { id=model.DepartmentID }); // vrati se na listu zaposlenih
+            }
+            // nije validan => vrati se na formu 
+            return View(model);
         }
     }
 }
