06eef16fbe0b69e047cfbdaccb5577bd244ac5cd
Erkan Islamovic <erkan@codex.ba>
2026-03-19 14:04:46 +0100

    [FEATURE] Dodali smo poziv SP za prikaz sumarne statistike odjela

--- BHAS/Controllers/StoredProcController.cs
+++ BHAS/Controllers/StoredProcController.cs
@@ -1,9 +1,12 @@
 ﻿using BHAS.DbFirst;
+using BHAS.Models;
+using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
 
@@ -72,6 +75,61 @@ namespace BHAS.Controllers
                 return View(employeess);
             }
         }
+
+        // [Stats].[sp_GetDepartmentSummary]
+
+        public ActionResult DepartmentSummary()
+        {
+            using (var db = new StateStatisticsDBEntities())
+            {
+                var query = db.Database
+                    .SqlQuery<DepartmentSummaryViewModel>("[Stats].[sp_GetDepartmentSummary]");
+
+                var summaryStat = query.ToList();
+
+                return View(summaryStat);
+            }
+        }
+
+
+        public ActionResult SalaryRaise()
+        {
+            using(var db = new StateStatisticsDBEntities())
+            {
+                PopulateDepartmentSelect(db);
+                return View();
+
+            }
+        }
+
+        /// <summary>
+        /// POST - update podataka u bazi
+        /// </summary>
+        /// <param name="departmentId"></param>
+        /// <param name="percentage"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult SalaryRaise(int departmentId, decimal percentage)
+        {
+            using(var db = new StateStatisticsDBEntities())
+            {
+                var p1 = new SqlParameter("@DepartmentID", departmentId);
+                var p2 = new SqlParameter("@PercentageIncrease", percentage);
+
+                var count = db.Database
+                    .ExecuteSqlCommand("[Stats].[sp_GiveSalaryRaise] @DepartmentID, @PercentageIncrease", 
+                    p1, p2);
+                //
+                ViewBag.RowsAffected = count;
+                ViewBag.PercentageIncrease = percentage;
+                //
+                PopulateDepartmentSelect(db, departmentId);
+                //
+                return View();
+            }
+        }
+
+
 
         private void PopulateDepartmentSelect(StateStatisticsDBEntities db, int? selectedDepartmentId = null)
         {
---
+++ BHAS/Models/DepartmentSummaryViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BHAS.DbFirst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BHAS.Models
+{
+    public class DepartmentSummaryViewModel
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+
+        public string DepartmentCode { get; set; }
+        public decimal Budget { get; set; }
+
+        public string ManagerFullName { get; set; }
+
+        public int EmployeeCount { get; set; }
+
+        public decimal AverageSalary { get; set; }
+
+        public decimal TotalSalaryBill { get; set; }
+
+        public int TotalProjectCount { get; set; }
+
+        public int ActiveProjectCount { get; set; }
+
+        public decimal TotalProjectBudget { get; set; }
+    }
+}
